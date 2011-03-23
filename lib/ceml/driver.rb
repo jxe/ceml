5a1ffd88b625ca40b911e23e069b90214520cd53
Joe Edelman <joe@citizenlogistics.com>
2011-03-23 12:44:21 -0400

    clean up old driver casting stuff

--- lib/ceml/driver.rb
+++ lib/ceml/driver.rb
@@ -45,15 +45,6 @@ module CEML
       SCRIPTS[script_collection_id] << script
     end
 
-    def ping_all script_collection_id, candidate, release = false
-      candidate[:script_collection_id] = script_collection_id
-      SCRIPTS[script_collection_id].each{ |s| ping script_collection_id, s.roles_to_cast, candidate, release }
-    end
-
-    def released_from_all script_collection_id, candidate
-      SCRIPTS[script_collection_id].each{ |s| release script_collection_id, s.roles_to_cast, candidate }
-    end
-
     def release p
       p[:tags] -= ['new']
       if p[:released] =~ /^(\w+)=/
@@ -78,69 +69,6 @@ module CEML
     LOCATIONS = Hash.new{ |h,k| h[k] = [] }
     def with_confluences script_collection_id, roleset
       yield LOCATIONS["#{script_collection_id}:#{roleset.hash}"]
-    end
-
-    def ping script_collection_id, roleset, candidate, involvement = :sticky
-      return unless roleset.any?{ |r| r.fits? candidate }
-      candidate[:ts] = CEML.clock
-      already_launched_with = nil
-      run_after = []
-
-      with_confluences script_collection_id, roleset do |confluences|
-        already_launched_with = nil
-        # live_with = confluences.select{ |c| c.live_with?(candidate) }
-        # if not live_with.empty?
-        #   already_launched_with = live_with.first.incident_id
-        #   live_with.each{ |c| c.rm candidate } if involvement != :sticky
-        #   break if involvement != :released
-        # end
-
-        locs = confluences.group_by{ |l| l.stage_with_candidate(candidate) }
-        if locs[:joinable]
-          log "joining..."
-          first = locs[:joinable].shift
-          first.push candidate unless first.incident_id == already_launched_with
-          # JOIN THEM
-          run_after << [:push, first.incident_id, nil, candidate]
-
-        elsif locs[:launchable]
-          log "launching..."
-          first = locs[:launchable].shift
-          first.push candidate
-          cast = first.cast
-          first.incident_id = gen_code
-          (locs[:launchable] + (locs[:listable]||[])).each{ |l| l.rm *cast }
-          # LAUNCH
-          run_after << [:launch, first.incident_id, script_collection_id, roleset, *cast]
-
-        elsif locs[:listable]
-          log "listing..."
-          locs[:listable].each{ |l| l.push candidate }
-
-        else
-          c = Confluence.new(roleset)
-          log "start-listing..."
-          if c.stage_with_candidate(candidate) == :launchable
-            log "start-launching..."
-            c.push candidate
-            c.incident_id = gen_code
-            run_after << [:launch, c.incident_id, script_collection_id, roleset, candidate]
-          else
-            c.push candidate
-          end
-          confluences << c
-        end
-        confluences.delete_if(&:over?)
-      end
-
-      run_after.each do |cmd|
-        send(*cmd)
-      end
-
-      # if already_launched_with and involvement == :sticky
-      #   puts "PUSHING INSTEAD"
-      #   push already_launched_with, nil, candidate
-      # end
     end
 
     def push incident_id, script, *updated_players
